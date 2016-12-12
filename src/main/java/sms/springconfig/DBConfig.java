2d57d5c69ab4d4b731d696506fc4e3cfd0764970
sshyun33 <sshyun33@gmail.com>
2016-12-12 17:24:42 +0900

    initialize project structure

---
+++ src/main/java/sms/springconfig/DBConfig.java
@@ -0,0 +1,73 @@
+package sms.springconfig;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DBConfig {
+    @Value("classpath:sql/initial/schema.sql")
+    private Resource H2_SCHEMA_SCRIPT;
+
+    @Value("classpath:sql/initial/data.sql")
+    private Resource H2_DATA_SCRIPT;
+
+    @Bean
+    DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("org.h2.Driver");
+        ds.setUrl("jdbc:h2:~/sshyun33;AUTO_SERVER=TRUE");
+        ds.setUsername("sa");
+        ds.setPassword("123");
+        return ds;
+    }
+
+    @Autowired
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
+
+        final DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator());
+//        initializer.setDatabaseCleaner(databaseCleaner());
+        return initializer;
+    }
+
+    private DatabasePopulator databasePopulator() {
+        final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(H2_SCHEMA_SCRIPT);
+        populator.addScript(H2_DATA_SCRIPT);
+        return populator;
+    }
+
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(dataSource());
+        sqlSessionFactory.setMapperLocations(
+                new PathMatchingResourcePatternResolver()
+                        .getResources("classpath:sql/mapper/*.xml"));
+        return sqlSessionFactory.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSession(SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
