c513722ce5b332b121b492d5f56037d473f3fa37
sshyun33 <sshyun33@gmail.com>
2016-12-15 13:04:38 +0900

    + 커넥션풀 변경 (Common DHCP -> HikariCP) + 스프링 트랜잭션 지원 추가 (애노테이션 방식) + 빌드스크립트 수정 (log4jdbc-log4j2 버전을 변수로 변경)

--- src/main/java/sms/springconfig/DBConfig.java
+++ src/main/java/sms/springconfig/DBConfig.java
@@ -1,22 +1,29 @@
 package sms.springconfig;
 
-import org.apache.commons.dbcp.BasicDataSource;
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.DatabasePopulator;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableTransactionManagement
 @PropertySources({
         @PropertySource(value = "classpath:properties/application-dev.properties"),
         @PropertySource(value = "classpath:properties/application-${spring.profiles.active}.properties",
@@ -33,21 +40,31 @@ public class DBConfig {
     @Value("${db.init.data}")
     private Resource INIT_DATA_SCRIPT;
 
+    // DataSource
     @Bean
     DataSource dataSource() {
-        BasicDataSource ds = new BasicDataSource();
+        HikariDataSource ds = new HikariDataSource();
         ds.setDriverClassName(env.getProperty("db.driver"));
-        ds.setUrl(env.getProperty("db.url"));
+        ds.setJdbcUrl(env.getProperty("db.url"));
         ds.setUsername(env.getProperty("db.username"));
         ds.setPassword(env.getProperty("db.password"));
-        ds.setInitialSize(2);
-        ds.setMaxActive(30);
-        ds.setMaxIdle(10);
-        ds.setMinIdle(3);
-        ds.setMaxWait(30000);
-        ds.setRemoveAbandoned(true);
-        ds.setRemoveAbandonedTimeout(30);
-        ds.setValidationQuery("SELECT 1");
+
+        // 최대 커넥션 개수 (기본값: 10개) - 가장 중요
+        ds.setMaximumPoolSize(10);
+        // 커넥션 타임아웃 값 (기본값: 30초)
+        ds.setConnectionTimeout(30000);
+        // 커넥션 최대 생명 시간 (기본값: 30분), 30초 이상 DB 기본 타임아웃 값 이하로 설정
+        ds.setMaxLifetime(1800000);
+        // 유휴 커넥션 타임아웃 값 (기본값: 10분), MinimumIdle값이 MaximumPoolSize이하로 설정되어있을 때만 유효
+        ds.setIdleTimeout(600000);
+        // 최소 유휴 커넥션 개수 (기본값: MaximumPoolSize), 설정안하는 것을 권장
+        // ds.setMinimumIdle(10);
+
+        // Caching (MySQL Recommended)
+        ds.addDataSourceProperty("cachePrepStmts", "true");
+        ds.addDataSourceProperty("prepStmtCacheSize", "250");
+        ds.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        ds.addDataSourceProperty("useServerPrepStmts", "true");
         return ds;
     }
 
@@ -68,7 +85,13 @@ public class DBConfig {
         return populator;
     }
 
-    // Mybatis beans
+    // Transaction
+    @Bean
+    public PlatformTransactionManager transactionManager(final DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    // Mybatis
     @Bean
     public SqlSessionFactory sqlSessionFactory(final DataSource dataSource) throws Exception {
         SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
