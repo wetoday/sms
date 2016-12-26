2d8055cf78bdbba04462381bc653e9cc1e5d2b63
sshyun33 <sshyun33@gmail.com>
2016-12-27 01:39:00 +0900

    스프링 로케일 설정 주석 변경

--- src/main/java/sms/spring/config/I18nConfig.java
+++ src/main/java/sms/spring/config/I18nConfig.java
@@ -17,7 +17,6 @@ import java.util.Locale;
  *       (단, Resolver의 DefaultLocale이 설정되어 있을 경우 해당 값으로 덮어쓰기)
  *   1.2) 두번째 요청부터는 로케일 리졸버의 방식에 따라 세션 또는 쿠키에서 로케일 확인
  * 2. 로케일에 맞는 메세지 파일의 값을 호출
- *   - 주의) ko => messages_ko.properties, ko_KR => messages_ko_KR.properties에 정확히 매칭됨
  * 3. 로케일에 맞는 메세지 파일이 없는 경우
  *   3.1) FallBackToSystemLocale이 true(기본값)인 경우
  *     3.1.1) 시스템 로케일에 맞는 메세지 파일의 값을 호출
